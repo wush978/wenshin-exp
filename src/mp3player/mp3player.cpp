6762d60a318fae7f61c1f2b33da4dabbe12f7d77
wush978 <wush978@gmail.com>
2012-04-27 00:17:01 +0800

    successfully play sound

--- src/mp3player/mp3player.cpp
+++ src/mp3player/mp3player.cpp
@@ -9,6 +9,8 @@
 #include <stdio.h>
 #include <conio.h>
 
+#include <iostream>
+
 #include <fmod.hpp>
 #include <fmod_errors.h>
 
@@ -29,84 +31,31 @@ int main(int argc, char* argv[])
 	}
 	const int argv_file_path(1);
 
-    FMOD::System     *system;
-    FMOD::Sound      *sound;
-    FMOD::Channel    *channel = 0;
-    FMOD_RESULT       result;
-//    int               key;
-    unsigned int      version;
+	FMOD_RESULT result;
+	FMOD::System *system;
 
-    /*
-        Create a System object and initialize.
-    */
-    result = FMOD::System_Create(&system);
-    ERRCHECK(result);
-
-    result = system->getVersion(&version);
-    ERRCHECK(result);
-
-    if (version < FMOD_VERSION)
-    {
-        printf("Error!  You are using an old version of FMOD %08x.  This program requires %08x\n", version, FMOD_VERSION);
-        return 0;
-    }
-
-    result = system->init(32, FMOD_INIT_NORMAL, 0);
-    ERRCHECK(result);
-
-    result = system->createSound(argv[argv_file_path], FMOD_HARDWARE, 0, &sound);
-    ERRCHECK(result);
-
-    result = sound->setMode(FMOD_LOOP_OFF);    /* drumloop.wav has embedded loop points which automatically makes looping turn on, */
-    ERRCHECK(result);                           /* so turn it off here.  We could have also just put FMOD_LOOP_OFF in the above CreateSound call. */
-
-    result = system->playSound(FMOD_CHANNEL_FREE, sound, false, &channel);
-    ERRCHECK(result);
-
-    system->update();
-
-	unsigned int ms = 0;
-	unsigned int lenms = 0;
-	bool         playing = 0;
-	bool         paused = 0;
-	int          channelsplaying = 0;
-
-	if (channel)
+	result = FMOD::System_Create(&system);
+	if (result != FMOD_OK)
 	{
-		FMOD::Sound *currentsound = 0;
-
-		result = channel->isPlaying(&playing);
-		if ((result != FMOD_OK) && (result != FMOD_ERR_INVALID_HANDLE) && (result != FMOD_ERR_CHANNEL_STOLEN))
-		{
-			ERRCHECK(result);
-		}
-
-		result = channel->getPaused(&paused);
-		if ((result != FMOD_OK) && (result != FMOD_ERR_INVALID_HANDLE) && (result != FMOD_ERR_CHANNEL_STOLEN))
-		{
-			ERRCHECK(result);
-		}
-
-		result = channel->getPosition(&ms, FMOD_TIMEUNIT_MS);
-		if ((result != FMOD_OK) && (result != FMOD_ERR_INVALID_HANDLE) && (result != FMOD_ERR_CHANNEL_STOLEN))
-		{
-			ERRCHECK(result);
-		}
-
-		channel->getCurrentSound(&currentsound);
-		if (currentsound)
-		{
-			result = currentsound->getLength(&lenms, FMOD_TIMEUNIT_MS);
-			if ((result != FMOD_OK) && (result != FMOD_ERR_INVALID_HANDLE) && (result != FMOD_ERR_CHANNEL_STOLEN))
-			{
-				ERRCHECK(result);
-			}
-		}
+	    printf("FMOD error! (%d) %s\n", result, FMOD_ErrorString(result));
 	}
 
-	system->getChannelsPlaying(&channelsplaying);
+	result = system->init(100, FMOD_INIT_NORMAL, 0);
+	if (result != FMOD_OK)
+	{
+	    printf("FMOD error! (%d) %s\n", result, FMOD_ErrorString(result));
+	}
 
-	printf("Time %02d:%02d:%02d/%02d:%02d:%02d : %s : Channels Playing %2d\r", ms / 1000 / 60, ms / 1000 % 60, ms / 10 % 100, lenms / 1000 / 60, lenms / 1000 % 60, lenms / 10 % 100, paused ? "Paused " : playing ? "Playing" : "Stopped", channelsplaying);
+	FMOD::Sound *sound;
+	result = system->createSound(argv[argv_file_path], FMOD_DEFAULT, 0, &sound);        // FMOD_DEFAULT uses the defaults.  These are the same as FMOD_LOOP_OFF | FMOD_2D | FMOD_HARDWARE.
+	ERRCHECK(result);
+
+	FMOD::Channel *channel;
+	result = system->playSound(FMOD_CHANNEL_FREE, sound, false, &channel);
+	ERRCHECK(result);
+
+	std::cout << "Press return to quit." << std::endl;
+	std::cin.get();
 
     /*
         Shut down
