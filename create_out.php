bb3533fa20317ae3cebdfb82dfe7e5fcdc90463b
Wush Wu <wush978@gmail.com>
2012-05-13 00:09:41 +0800

    update

--- create_out.php
+++ create_out.php
@@ -6,10 +6,10 @@ foreach ($config_files_raw as $file_name) {
 	}
 	$hash = substr($file_name, 7, 32);
 	$config_path = 'exam_config/' . $file_name;
-	$out = 'out';
+	$out = 'out_' . $hash;
 	$command = 'php app/console exam:generate vendor/wenshin/ ' . $config_path . ' ' . $out . ' ' . $hash;
 	echo $command . "\n";
 	echo system($command) . "\n";
+	echo system('rm -rf out_' . $hash . '/.git') . "\n";
 }
-system('rm -rf out/.git');
 ?>
--- src/Exam/GeneratorBundle/Resources/ExamTemplate.php
+++ src/Exam/GeneratorBundle/Resources/ExamTemplate.php
@@ -12,12 +12,12 @@ class ExamTemplate
 	var hash = "%hash%";
 	var Result = new Array(%question_size%);
 	var Question = new Array(%question_size%);
-	var Sound_src = new Array(%question_size%);
 	var question_index = 0;
 	var question_size = %question_size%;
 	
 	function nextQuestion() {
-		is_play = false;
+		stop();
+		played_time = 0;
 		question_index++;
 		if (question_index < question_size) {
 			document.body.innerHTML = Question[question_index];
@@ -31,12 +31,13 @@ class ExamTemplate
 				}
 			}
 			var fso = new ActiveXObject("Scripting.FileSystemObject");
-			var file_name = class_id + "_" + student_id + "_" + hash + ".txt";
+			var file_name = school + "_" + level + "_" + class_var + "_" + student_id + "_" + hash + ".txt";
 			var location = document.URL;
 			location = location.slice(7, location.length - 41);
 			location = location.concat(file_name);
 			var s = fso.CreateTextFile(location, true);
 			s.WriteLine(result);
+			s.WriteLine(personal_data);
 			s.Close();
 			document.body.innerHTML = "謝謝你的作答，請將「" + file_name + "」放到老師指定的位置。" ;
 		}	
@@ -58,19 +59,23 @@ class ExamTemplate
 			}
 		}
 	}
-	function play() { 
-		if (is_play) { 
-			return false; 
-		} 
-		is_play = true; 
-		var node = document.createElement("embed"); 
-		node.setAttribute("src",Sound_src[question_index]); 
-		node.setAttribute("autostart","TRUE"); 
-		node.setAttribute("hidden","TRUE");  
-		document.body.appendChild(node); 
-		return true; 
+	var played_time;
+	function play() {
+		if (played_time >= 2) {
+			return false;
+		}
+		played_time++;
+		document.embeds[0].stop(); 
+		alert("這次是聽第" + played_time + "次喔，總共只能聽兩次");
+    	document.embeds[0].play(); 
+		
+		return true;
 	} 
-	var is_play = false;
+	function stop() {
+		if (played_time > 0) {
+			document.embeds[0].stop();
+		}
+	}
 	//%assign_Question%
 	//%assign_Sound_src%
 	
@@ -78,21 +83,128 @@ class ExamTemplate
 		alert(document.body.innerHTML.toString());
 	}
 	
-	var class_id;
+	var school;
+	var level;
+	var class_var;
 	var student_id;
+	var sex;
+	var is_learned_music;
+	var start_music_year_old;
+	var music_subject;
+	var how_long_learn_music;
+	var is_join_music_group;
+	var joined_music_group_name;
+	var is_join_music_cram_school;
+	var music_cram_school_name;
+	var music_cram_school_subject;
+	var personal_data;
 	function startTest() {
-		var temp;
-		class_id = document.getElementById("class_id").value;
-		student_id = document.getElementById("student_id").value;
-		temp = "你的班級: " + class_id + "<br/>";
-		temp = temp + "你的座號: " + student_id + "<br/>";
-		temp = temp + \'<input type="button" onclick="nextQuestion()" value="是"/><br/>\';
-		temp = temp + \'<input type="button" onclick="beginPage()" value="否"/><br/>\';
-		document.body.innerHTML = temp;  
+		school = document.getElementById("input_school").value;
+		level = document.getElementById("input_level").value;
+		class_var = document.getElementById("input_class").value;
+		student_id = document.getElementById("input_student_id").value;
+		if (document.getElementById("input_sex_boy").checked) {
+			sex = "男生";
+		}
+		if (document.getElementById("input_sex_girl").checked) {
+			sex = "女生";
+		}
+		if (document.getElementById("is_learned_music_no").checked) {
+			is_learned_music = "無";
+			start_music_year_old = "NA";
+			music_subject = "NA";
+			how_long_learn_music = "NA";
+		}
+		if (document.getElementById("is_learned_music_yes").checked) {
+			is_learned_music = "有";
+			start_music_year_old = document.getElementById("input_start_music_year_old").value;
+			music_subject = document.getElementById("input_music_subject").value;
+			if (document.getElementById("input_how_long_learn_music1").checked) {
+				how_long_learn_music = "0-0.5";
+			}
+			if (document.getElementById("input_how_long_learn_music2").checked) {
+				how_long_learn_music = "0.5-1";
+			}
+			if (document.getElementById("input_how_long_learn_music3").checked) {
+				how_long_learn_music = "1-2";
+			}
+			if (document.getElementById("input_how_long_learn_music4").checked) {
+				how_long_learn_music = "2-3";
+			}
+		}
+		if (document.getElementById("input_is_join_music_group_no").checked) {
+			is_join_music_group = "無";
+			joined_music_group_name = "NA";
+		}
+		if (document.getElementById("input_is_join_music_group_yes").checked) {
+			is_join_music_group = "有";
+			joined_music_group_name = document.getElementById("input_joined_music_group_name").value;
+		}
+		if (document.getElementById("input_is_join_music_cram_school_no").checked) {
+			is_join_music_cram_school = "無參加課外自費音樂課程";
+			music_cram_school_name = "NA";
+			music_cram_school_subject = "NA";
+		}
+		if (document.getElementById("input_is_join_music_cram_school_yes_group").checked) {
+			is_join_music_cram_school = "有參加課外自費音樂團體課程";
+			music_cram_school_name = document.getElementById("input_music_cram_school_name").value;
+			music_cram_school_subject = "NA";
+		}
+		if (document.getElementById("input_is_join_music_cram_school_yes_single").checked) {
+			is_join_music_cram_school = "有參加課外自費音樂家教";
+			music_cram_school_name = "NA";
+			music_cram_school_subject = document.getElementById("input_music_cram_school_subject").value;
+		}
+		personal_data = \'1.國小:\' + level +
+			\'年級:\' + level +
+			\'班級:\' + class_var +
+			\'座號:\' + student_id + \'<br/>\' + 
+			\'<hr/>\'+
+			\'2.性別:\' + sex + \'<br/><hr/>\' +
+			\'3.\' + is_learned_music + \'學過獨奏樂器<br/>\' + 
+			\'從\' + start_music_year_old + \'歲開始,\' +
+			\'樂器名稱: \' + music_subject + \'<br/>\' +
+			\'學了\' + how_long_learn_music + \'年<br/><hr/>\' +
+			\'4.\' + is_join_music_group + \'參加過或目前正參加學校音樂性社團<br/>\' +
+			\'名稱:\' + joined_music_group_name + \'<br/><hr/>\' +
+			\'5.\' + is_join_music_cram_school + \'<br/>\' +
+			\'課程名稱: \' + music_cram_school_name + \'<br/>\' +
+			\'家教名稱: \' + music_cram_school_subject + \'<br/><hr/>\' +
+			\'<input type="button" onclick="nextQuestion()" value="資料正確"/><input type="button" onclick="beginPage()" value="資料錯誤，我要重新輸入"/>\';		
+		document.body.innerHTML = personal_data;  
 	}
 	
 	function beginPage() {
-		document.body.innerHTML = \'班級:<input id="class_id" type="text"/><br/>	座號:<input id="student_id" type="text"/><br/><input type="button" onclick="startTest()" value="開始作答"/>\';		
+		document.body.innerHTML = \'1.國小:<input id="input_school" type="text"/>\' +
+			\'年級:<input id="input_level" type="text"/>\' +
+			\'班級:<input id="input_class" type="text"/>\' +
+			\'座號:<input id="input_student_id" type="text"/><br/>\' + 
+			\'<hr/>\'+
+			\'2.性別:<br/><input type="radio" name="input_sex" value="boy" id="input_sex_boy">男生</input>\' +
+			\'<input type="radio" name="input_sex" value="girl" id="input_sex_girl">女生</input><br/><hr/>\' +
+			\'3.請問你曾經學過或目前正在學獨奏樂器嗎?<br/>\' + 
+			\'<input type="radio" name="input_is_learned_music" value="no" id="is_learned_music_no">無</input>(請跳答第4題)<br/>\' +
+			\'<input type="radio" name="input_is_learned_music" value="yes" id="is_learned_music_yes">有</input>, \' +
+			\'我從<input id="input_start_music_year_old" type="text"/>歲開始,\' +
+			\'樂器名稱: <input id="input_music_subject" type="text"/><br/>\' +
+			\'學了多久?<br/>\' +
+			\'<input type="radio" name="input_how_long_learn_music" value="0-0.5" id="input_how_long_learn_music1">半年以內</input><br/>\' +
+			\'<input type="radio" name="input_how_long_learn_music" value="0.5-1" id="input_how_long_learn_music2">半年到一年</input><br/>\' +
+			\'<input type="radio" name="input_how_long_learn_music" value="1-2" id="input_how_long_learn_music3">一年到兩年</input><br/>\' +
+			\'<input type="radio" name="input_how_long_learn_music" value="2-3" id="input_how_long_learn_music4">兩年到三年</input><br/><hr/>\' +
+			\'4.你是否參加過或目前正參加學校音樂性社團？(例如：合唱團、節奏樂隊、管樂團、國樂團、節令鼓)<br/>\' +
+			\'<input type="radio" name="input_is_join_music_group" value="no" id="input_is_join_music_group_no">無</input><br/>\' +
+			\'<input type="radio" name="input_is_join_music_group" value="yes" id="input_is_join_music_group_yes">有</input>, \' +
+			\'名稱:<input type="text" id="input_joined_music_group_name"/><br/><hr/>\' +
+			\'5.你是否參加過或目前仍參加課外自費的音樂課程?<br/>\' +
+			\'<input type="radio" name="input_is_join_music_cram_school" value="no" id="input_is_join_music_cram_school_no">無</input><br/>\' +
+			\'<input type="radio" name="input_is_join_music_cram_school" value="yes_group" id="input_is_join_music_cram_school_yes_group">有</input>, \' +
+			\'音樂團體課，例如「山葉團體鍵盤課」、「朱宗慶打擊樂團」等…<br/>\' +
+			\'(請說明課程名稱: <input type="text" id="input_music_cram_school_name"/>)<br/>\' +
+			\'<input type="radio" name="input_is_join_music_cram_school" value="yes_single" id="input_is_join_music_cram_school_yes_single">有</input>, \' +
+			\'音樂家教，例如鋼琴、小提琴、長笛、吉他等…<br/>\' +
+			\'(請說明課程名稱: <input type="text" id="input_music_cram_school_subject"/>)<br/><hr/>\' +
+			\'<input type="button" onclick="startTest()" value="開始作答"/>\';		
 	}
 	
 </script>
@@ -114,7 +226,9 @@ class ExamTemplate
 <input type="button" value="除錯" onclick="debug()"/><br/>
 %question_description%<br/>
 <hr>
-<a id="play" href="#" onclick="play()" style="" >播放</a><br/> 
+<input type="button" value="播放" id="play" onclick="play()" style="" />
+<embed src="%sound_src%" hidden="true" autostart="false" />
+<br/> 
 <hr>
 <form id="form" action="%next_question%.html" method="post"> 
 	%options% 
@@ -132,6 +246,7 @@ class ExamTemplate
 	    
 	    $this->body = str_replace('%question_title%', $question->getTitle(), self::$body_template);
 	    $this->body = str_replace('%question_description%', $question->getDescription(), $this->body);
+		$this->body = str_replace('%sound_src%', $question->getSound(), $this->body);
 	    $options = '';
 	    foreach($question->getOptions() as $option) {
 	        /* @var Option $option */
@@ -147,7 +262,7 @@ class ExamTemplate
 	    $this->assign_Question .= "\n\t//%assign_Question%\n";
 	    self::$html_template = str_replace('//%assign_Question%', $this->assign_Question, self::$html_template);
 	    
-	    $this->sound_src = 'Sound_src[' . $question->getTitle() . '] = ' . "'" . $question->getSound() . "';\n\t//%assign_Sound_src%\n";
+//	    $this->sound_src = 'Sound_src[' . $question->getTitle() . '] = ' . "'" . $question->getSound() . "';\n\t//%assign_Sound_src%\n";
         self::$html_template = str_replace('//%assign_Sound_src%', $this->sound_src, self::$html_template);	     
 	    
 	    self::$question_size++;
